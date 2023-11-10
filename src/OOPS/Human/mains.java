0e9e20f4761cd244285a189455b74465a91ce76d
Anitya Sharma <anityas4@gmail.com>
2023-11-10 18:17:08 +0530

    First Commit

---
+++ src/OOPS/Human/mains.java
@@ -0,0 +1,13 @@
+package OOPS.Human;
+
+public class mains {
+    public static void main(String[] args) {
+        Human Anitya = new Human(25,"Anitya",10000,false);
+        Human Rahul = new Human(26,"Rahul",15000,true);
+        Human arpit = new Human(26,"arpit",15000,true);
+
+        System.out.println(Human.population);
+        System.out.println(Human.population);
+        System.out.println(Human.population);
+    }
+}
